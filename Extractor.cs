b66dad1e7a8ad2cba52a90e48d0e41299f944179
Whilda Chaq <whilda.chaq@divusi.com>
2014-08-03 16:02:28 +0700

    Back Up TA 3 Agustus 2014

---
+++ Extractor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+using System.Windows.Markup;
+using SourceAFIS.Simple;
+using System.IO;
+using System.Diagnostics;
+
+namespace TestAPIFingerprint
+{
+    class Extractor
+    {
+        static AfisEngine AFIS = new AfisEngine();
+
+        static void Main(string[] args)
+        {
+            String fvc = "FVC2006";
+            String db = "Db1_a"; // 1
+            String prefix = fvc + "-" + db;
+            String pathImage = "E:/College/Semester 7/Tentang TA/Oprek FVC/DVD/"+fvc+"/Dbs/"+db;
+            String pathIso = "E:/College/Semester 7/Tentang TA/Oprek FVC/DVD/"+fvc+"/Dbs/Temp-"+db;
+            Directory.CreateDirectory(pathIso);
+            String[] files = Directory.GetFiles(pathImage);
+            int i = 0;
+            Stopwatch watch = Stopwatch.StartNew();
+            foreach(String uriFp in files){
+                try
+                {
+                    Fingerprint fp = new Fingerprint();
+                    fp.AsBitmapSource = new BitmapImage(new Uri(uriFp, UriKind.RelativeOrAbsolute));
+                    Person ps = new Person();
+                    ps.Fingerprints.Add(fp);
+                    AFIS.Extract(ps);
+                    File.WriteAllBytes(pathIso + "/" + prefix + "_"+ Path.GetFileNameWithoutExtension(uriFp)+".template", fp.AsIsoTemplate);
+                    Console.WriteLine("Progress : "+ (((float) (++i)/files.Length) * 100)+" %");
+                }catch(Exception ex){
+                    Console.WriteLine(" Skip!!"+ex.Message);
+                }
+            }
+            watch.Stop();
+            String [] lines = new String[1];
+            lines[0] = "Time : " + watch.ElapsedMilliseconds + " ms";
+            File.AppendAllLines(pathIso + "/properties.txt", lines);
+            Console.WriteLine("Time : "+ watch.ElapsedMilliseconds + " ms");
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ Matching.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+using System.Windows.Markup;
+using SourceAFIS.Simple;
+using System.IO;
+
+namespace TestAPIFingerprint
+{
+    class Matching
+    {
+        static AfisEngine AFIS = new AfisEngine();
+
+        static void Main(string[] args)
+        {
+            Person queryPerson = null;
+            //  String path = "E:/College/Semester 7/Tentang TA/Oprek FVC/Template/FVC2000/Db1_a/FVC2000-Db1_a_1_1.template";
+            String path = "E:/College/Semester 7/Tentang TA/Oprek FVC/Template/Summary/FVC2000-Db1_a_1_1.template";
+            queryPerson = SetupQueryPerson(queryPerson,path);
+
+            Fingerprint fp = new Fingerprint();
+            Person dataPerson = new Person(fp);
+            float verify;
+            
+
+            String fvc = "FVC2000";
+            String db = "Db1_a";
+            String prefix = fvc + "-" + db;
+            //String pathTemplate = "E:/College/Semester 7/Tentang TA/Oprek FVC/Template/" + fvc + "/" + db;
+            String pathTemplate = "E:/College/Semester 7/Tentang TA/Oprek FVC/Template/Summary";
+            String[] files = Directory.GetFiles(pathTemplate);
+
+            int counter = 0;
+            foreach(String uriTmp in files){
+                if (!Path.GetFileNameWithoutExtension(uriTmp).Equals("properties"))
+                {
+                    fp.AsIsoTemplate = File.ReadAllBytes(uriTmp);
+                    verify = AFIS.Verify(queryPerson, dataPerson);
+                    if(verify!=0)
+                        File.AppendAllText("c:/infoPenting",Path.GetFileName(uriTmp) + "\t" + verify+"\n");
+                }
+                Console.WriteLine("Progress = "+((float) ++counter/files.Length)*100 +" %");
+            }
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static Person SetupQueryPerson(Person queryPerson,String path)
+        {
+            Fingerprint fp = new Fingerprint();
+            fp.AsIsoTemplate = File.ReadAllBytes(path);
+            return new Person(fp);
+        }
+    }
+}
